c4d7dc3b552aa0d94116ba0b80e962e5c43670e8
Aengus Walton <ventolin@gmail.com>
2014-03-15 11:34:50 +0100

    First commit, first sketches..

---
+++ scrapers.py
@@ -0,0 +1,90 @@
+import logbook
+
+log = logbook.Logger()
+
+
+class GenericScraper(object):
+    def __init__(self, date):
+        self.date = date
+
+    def scrape(self):
+        """Scrape tracks for a single date"""
+        raise NotImplementedError
+
+    def time_to_datetime(self, text_time, split_char):
+        """Transform a text representation of time into a datetime"""
+        text_time = text_time.split(split_char)
+        hour = text_time[0]
+        minute = text_time[1]
+        try:
+            second = text_time[2]
+        except IndexError:
+            second = 0
+        return self.date.replace(hour=hour, minute=minute, second=second)
+
+
+class SWR1BWScraper(GenericScraper):
+    base_url = ('http://www.swr.de/swr1/bw/musik/musikrecherche/-/id=446260'
+                '/8biejp/index.html')
+    start_date = '20140224'
+
+    @property
+    def tracklist_urls(self):
+        return [x['value'] for x in self.soup.find(
+            'select', {'id': 'uhrzeit'}).findAll('option')]
+
+    def extract_tracks(self):
+        """Parse HTML of a tracklist page and return a list of 
+        (artist, title, time played) tuples
+        """
+        main_div = self.soup.find('div', {'class': 'recherchelist'})
+        elements = main_div.findAll(
+            'p', {'class': ['sendezeitrl', 'songtitel']})
+        i = 1
+        for time_tag in elements[::2]:
+            self.tracks.append(
+                (elements[i].span.text, elements[i].a.text, self.time_to_datetime(time_tag, '.')))
+            i += 2
+
+    def scrape(self):
+        resp = requests.get(self.base_url)
+        soup = BeautifulSoup(resp.text)
+        date_links = soup.findAll('a', {'class': 'pgcalendarblue'})
+        for url in [tag['href'] for tag in date_links]:
+            if 'date={0}'.format(self.date.strftime('%Y%m%d')) in url:
+                resp = requests.get(url)
+                self.soup = BeautifulSoup(resp.text)
+                self.extract_tracks()
+                return
+        raise LookupError
+
+
+class SWR3Scraper(GenericScraper):
+    base_url = ('http://www.swr3.de/musik/playlisten/Musikrecherche-Playlist-Was-lief'
+                '-wann-auf-SWR3/-/id=47424/cf=42/did=65794/93avs/index.html'
+                '?hour={hour}&date={date}')
+    start_date = '20130301'
+
+    @property
+    def tracklist_urls(self):
+        return [self.base_url.format(hour=i, date=self.date.strftime('%Y%m%d')) for i in range(24)]
+
+    def extract_tracks(self):
+        """Parse HTML of a tracklist page and return a list of 
+        (artist, title, time played) tuples
+        """
+        table = self.soup.find('table', {'class': 'richtext'})
+        for row in table.findAll('tr'):
+            elements = row.findAll('td')
+            if not elements:
+                continue
+            self.tracks.append(
+                (elements[0].text, elements[1].text,
+                    self.time_to_datetime(elements[2].text, ':')))
+
+    def scrape(self):
+        for url in self.tracklist_urls:
+            resp = requests.get(url)
+            self.soup = BeautifulSoup(resp.text)
+            self.extract_tracks()
+
