a8af7498cfef158fd66f090e6f681c87296f3713
Aengus Walton <Aengus.Walton@blue-yonder.com>
2014-12-10 12:39:42 +0100

    Remove self from http_get

--- lib.py
+++ lib.py
@@ -7,7 +7,7 @@ USER_AGENT = ('Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 '
               '(KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36')
 
 
-def http_get(self, url, retries=10, user_agent=USER_AGENT, cookies=None):
+def http_get(url, retries=10, user_agent=USER_AGENT, cookies=None):
     """Wrapper for requests.get for retries"""
     if cookies is None:
         cookies = {}
@@ -17,7 +17,7 @@ def http_get(self, url, retries=10, user_agent=USER_AGENT, cookies=None):
                 url, headers={'User-Agent': user_agent}, cookies=cookies)
         except Exception as e:
             time.sleep(1)
-            return self.http_get(url, retries=retries-1)
+            return http_get(url, retries=retries-1)
         else:
             return retval
     else:
