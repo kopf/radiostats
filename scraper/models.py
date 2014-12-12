dddd8a8202e331250468159fb20c38e1a2eae89c
Aengus Walton <Aengus.Walton@blue-yonder.com>
2014-12-12 16:37:44 +0100

    First version of db schema, settings schema

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Play(models.Model):
+    song = models.ForeignKey(Song)
+    time = models.DateTimeField()
+
+
+class Song(models.Model):
+    artist = models.TextField(null=False)
+    title = models.TextField(null=False)
+    normalized = models.ForeignKey(NormalizedSong)
+
+
+class NormalizedSong(models.Model):
+    mbid = models.CharField(max_length=36, null=False)
+    artist = models.TextField(null=False)
+    title = models.TextField(null=False)
+    tags = models.ManyToManyField(Tag)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
