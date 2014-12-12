9f735cdb2b123324351f6d7cefa3d0c2cadf82e5
Aengus Walton <Aengus.Walton@blue-yonder.com>
2014-12-12 16:51:42 +0100

    Update models

--- scraper/models.py
+++ scraper/models.py
@@ -1,24 +1,23 @@
 from django.db import models
 
 
-class Play(models.Model):
-    song = models.ForeignKey(Song)
-    time = models.DateTimeField()
-
-
-class Song(models.Model):
-    artist = models.TextField(null=False)
-    title = models.TextField(null=False)
-    normalized = models.ForeignKey(NormalizedSong)
-
-
-class NormalizedSong(models.Model):
-    mbid = models.CharField(max_length=36, null=False)
-    artist = models.TextField(null=False)
-    title = models.TextField(null=False)
-    tags = models.ManyToManyField(Tag)
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=32)
 
+
+class NormalizedSong(models.Model):
+    mbid = models.CharField(max_length=36)
+    artist = models.TextField()
+    title = models.TextField()
+    tags = models.ManyToManyField(Tag, null=True)
+
+
+class Song(models.Model):
+    artist = models.TextField()
+    title = models.TextField()
+    normalized = models.ForeignKey(NormalizedSong, null=True)
+
+
+class Play(models.Model):
+    song = models.ForeignKey(Song)
+    time = models.DateTimeField()
