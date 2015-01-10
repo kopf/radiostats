3e4e915e313b28395b0c92c461fcf49086d5479d
Aengus Walton <Aengus.Walton@blue-yonder.com>
2015-01-10 19:27:19 +0100

    Try first to get track metadata with track.getinfo, fall back on track.search

--- scraper/management/commands/normalize.py
+++ scraper/management/commands/normalize.py
@@ -20,6 +20,9 @@ FILE_LOGGER = logbook.FileHandler(
 
 class Command(BaseCommand):
     help = 'Normalizes tracks with canonical song information from Last.fm'
+    base_url = (
+        u'http://ws.audioscrobbler.com/2.0/?method={method}&autocorrect=1'
+        u'&artist={artist}&track={title}&api_key={api_key}&format=json')
 
     def handle(self, *args, **options):
         FILE_LOGGER.push_thread()
@@ -30,12 +33,41 @@ class Command(BaseCommand):
             if i % 10 == 0:
                 log.info('Done {0}...'.format(i))
 
-    def get_info(self, artist, title):
-        url = (u'http://ws.audioscrobbler.com/2.0/?method=track.getinfo&autocorrect=1'
-               u'&artist={artist}&track={title}&api_key={api_key}&format=json')
-        url = url.format(artist=quote_plus(artist.encode('utf-8')),
-                         title=quote_plus(title.encode('utf-8')),
-                         api_key=settings.LASTFM_API_KEY)
+    def _get(self, url):
+        """An extra, clumsy wrapper for http_get for the last.fm api"""
+        try:
+            resp = http_get(url).json()
+        except JSONDecodeError:
+            try:
+                resp = http_get(url).json()
+            except JSONDecodeError:
+                log.error('Error occurred twice trying to parse response from {0}'.format(url))
+                return None
+        return resp
+
+    def getinfo(self, encoded_artist, encoded_title):
+        """Get metadata from last.fm using the track.getinfo method"""
+        url = self.base_url.format(
+            artist=encoded_artist, title=encoded_title,
+            api_key=settings.LASTFM_API_KEY, method='track.getinfo')
+        resp = self._get(url)
+        if not resp:
+            return None
+        if isinstance(resp, dict) and not 'error' in resp and 'track' in resp:
+            if not resp['track']['mbid']:
+                # Found an incomplete listing, better to use track.search
+                return None
+            else:
+                return resp['track']
+        else:
+            log.error('Invalid Last.fm response: {0}'.format(url))
+            return None
+
+    def tracksearch(self, encoded_artist, encoded_title):
+        """Get metadata from last.fm using the track.search method"""
+        url = self.base_url.format(
+            artist=encoded_artist, title=encoded_title,
+            api_key=settings.LASTFM_API_KEY, method='track.search')
         try:
             resp = http_get(url).json()
         except JSONDecodeError:
@@ -44,33 +76,74 @@ class Command(BaseCommand):
             except JSONDecodeError as e:
                 log.error('Error occurred twice trying to parse response from {0}'.format(url))
                 return None
-        if isinstance(resp, dict) and not 'error' in resp and 'track' in resp:
-            return resp['track']
+        if isinstance(resp, dict):
+            if (resp.get('results', {}).get('trackmatches')
+                    and not isinstance(resp['results']['trackmatches'], basestring)):
+                result = resp['results']['trackmatches']['track']
+                if isinstance(result, list):
+                    result = result[0]
+            else:
+                # Track not found by last.fm
+                result = None
         else:
             log.error('Invalid Last.fm response: {0}'.format(url))
-            return None
+            result = None
+        return result
+
+    def get_tags(self, mbid):
+        """Get tags from last.fm by using mbid of track we found using track.search"""
+        url = (u'http://ws.audioscrobbler.com/2.0/?method=track.getInfo'
+               u'&mbid={mbid}&api_key={api_key}&format=json')
+        url = url.format(mbid=mbid, api_key=settings.LASTFM_API_KEY)
+        resp = http_get(url).json()
+        if isinstance(resp, dict):
+            return resp.get('track', {}).get('toptags')
+        return []
+
+    def extract_tags(self, toptags):
+        """Extract tags from a 'toptags' value from a last.fm response"""
+        if isinstance(toptags, dict):
+            if isinstance(toptags.get('tag'), dict):
+                return [toptags['tag']['name']]
+            else:
+                return [tag['name'] for tag in toptags.get('tag', [])]
+        return []
+
+    def query_lastfm(self, artist, title):
+        """Use track.getinfo to get track metadata. If it fails, fall back on
+        track.search"""
+        encoded_artist = quote_plus(artist.encode('utf-8'))
+        encoded_title = quote_plus(title.encode('utf-8'))
+        track_info = self.getinfo(encoded_artist, encoded_title)
+        if not track_info:
+            track_info = self.tracksearch(encoded_artist, encoded_title)
+            if not track_info:
+                return None
+            fixed_artist = track_info['artist'][:256]
+            fixed_title = track_info['name'][:256]
+            mbid = track_info['mbid']
+            toptags = self.get_tags(mbid)
+        else:
+            fixed_artist = track_info['artist']['name'][:256]
+            fixed_title = track_info['name'][:256]
+            mbid = track_info['mbid']
+            toptags = track_info.get('toptags', [])
+        tags = self.extract_tags(toptags)
+        return {'artist': fixed_artist, 'title': fixed_title, 'mbid': mbid,
+                'tags': tags}
 
     def normalize(self, track):
         """Using last.fm's API, normalise the artist and track title"""
-        track_info = self.get_info(track.artist, track.title)
+        track_info = self.query_lastfm(track.artist, track.title)
         if not track_info:
             return
-        artist = track_info['artist']['name'][:256]
-        title = track_info['name'][:256]
-        mbid = track_info['mbid']
-        toptags = track_info.get('toptags', [])
-        if isinstance(toptags, dict):
-            if isinstance(toptags.get('tag', []), dict):
-                # only one tag
-                toptags = [toptags['tag']['name']]
-            else:
-                toptags = [tag['name'] for tag in toptags.get('tag', [])]
         try:
             normalized = NormalizedSong.objects.get(
-                mbid=mbid, artist=artist, title=title)
+                mbid=track_info['mbid'], artist=track_info['artist'],
+                title=track_info['title'])
         except ObjectDoesNotExist:
             tag_objects = []
-            for text_tag in toptags:
+            for text_tag in track_info['tags']:
                 try:
                     tag, _ = Tag.objects.get_or_create(name=text_tag)
                 except DataError:
@@ -79,9 +152,9 @@ class Command(BaseCommand):
                     continue
                 tag_objects.append(tag)
             normalized, _= NormalizedSong.objects.get_or_create(
-                mbid=mbid,
-                artist=artist,
-                title=title)
+                mbid=track_info['mbid'],
+                artist=track_info['artist'],
+                title=track_info['title'])
             normalized.tags.add(*tag_objects)
             normalized.save()
         track.normalized = normalized
