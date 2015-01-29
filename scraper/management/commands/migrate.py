9078c7121b22c8edceeaec61b4f83f519a3e92d3
Aengus Walton <Aengus.Walton@blue-yonder.com>
2015-01-29 13:41:19 +0100

    Add temporary migration script

---
+++ scraper/management/commands/migrate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from datetime import timedelta
+import logbook
+import pytz
+
+from django.core.management.base import BaseCommand
+
+from scraper.models import Station, Play
+
+
+log = logbook.Logger()
+
+
+class Command(BaseCommand):
+
+    help = ''
+
+    def handle(self, *args, **options):
+        i = 0
+        for station in Station.objects.all():
+            for play in Play.objects.filter(station=station):
+                play.time = play.local_time.astimezone(pytz.timezone(station.timezone))
+                play.save()
+                i += 1
+                if i % 100 == 0:
+                    print '{0} done'.format(i)
