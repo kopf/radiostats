ce8621c840fb080f6dd564659d136570de98f398
Aengus Walton <Aengus.Walton@blue-yonder.com>
2015-06-04 21:10:23 +0200

    Add play count to song admin

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
+from django.db import models
 
 from .models import Tag, Station, NormalizedSong, Song, Play
+
 
 class StationAdmin(admin.ModelAdmin):
     list_display = ('name', 'country', 'timezone', 'start_date', 'last_scraped', 'enabled')
 
+
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'title', 'normalized', 'last_scraped')
+    list_display = ('artist', 'title', 'normalized', 'last_scraped', 'play_count')
+
+    def queryset(self, request):
+        return Song.objects.annotate(play_ct=models.Count('play'))
+
+    def play_count(self, inst):
+        return inst.play_ct
+
+    play_count.admin_order_field = 'play_ct'
+
 
 class NormalizedSongAdmin(admin.ModelAdmin):
     list_display = ('mbid', 'artist', 'title')
 
+
 class PlayAdmin(admin.ModelAdmin):
     list_display = ('local_time', 'time', 'song', 'station')
-
 
 
 admin.site.register(Station, StationAdmin)
