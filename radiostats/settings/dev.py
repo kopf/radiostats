c5671baf3731df1f86560fe6ada6d66ebc2f7a35
Aengus Walton <ventolin@gmail.com>
2026-01-20 11:43:14 +0100

    Allow some level of concurrency using a timeout

--- radiostats/settings/dev.py
+++ radiostats/settings/dev.py
@@ -4,5 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'OPTIONS': {"timeout": 20, "transaction_mode": "IMMEDIATE"} # transaction_mode in later versions of django
+        'OPTIONS': {"timeout": 20}
     }
 }
