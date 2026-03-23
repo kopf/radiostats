6a926094543d257bf748e9c88f8b895cb25add26
Aengus Walton <ventolin@gmail.com>
2026-03-23 19:00:11 +0100

    Rewrite urls

---
+++ radiostats/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, re_path
+
+urlpatterns = [
+    # Examples:
+    # re_path(r'^$', 'radiostats.views.home', name='home'),
+    # re_path(r'^blog/', include('blog.urls')),
+
+    re_path(r'^admin/', include(admin.site.urls)),
+]
